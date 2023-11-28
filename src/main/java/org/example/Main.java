6906bee3d21ad8b343bfe66c409372beba3ed9de
Luzia Gabriela Abreu da Silva Santos <73603007+Luzia-Silva@users.noreply.github.com>
2023-11-28 09:59:33 -0300

    Criação de de um programa de analises imc

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+
+public class Main {
+	public static void main(String args[]){
+		double altura, peso, imc;
+		String resultado = "";
+		
+		altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a sua altura:"));
+		peso = Double.parseDouble(JOptionPane.showInputDialog("Digite a seu peso:"));
+		
+		imc = peso / (altura * altura);
+		
+		if(imc < 18.5) resultado = "abaixo do peso!";
+		else if (imc > 18.5 && imc < 24.9) resultado = "peso ideal!";
+		else resultado = "acima do peso!";
+		
+		DecimalFormat decimal = new DecimalFormat("0.00");
+		String imcFormatado = decimal.format(imc);
+		
+		
+		
+		JOptionPane.showMessageDialog(null, "Esse é seu IMC " + imcFormatado + " e você está " + resultado);
+	
+	}
+}
