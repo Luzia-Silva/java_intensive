d590a176e29aa9b1091ed69ba355e702d27b8486
Luzia-Silva <aluziagabriela@gmail.com>
2023-11-28 21:31:23 -0300

    Exercicios

---
+++ src/main/java/org/example/LandArea.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class LandArea {
+    public static void main(String args[]){
+        Double largura, comprimento, valor, area;
+        largura = Double.parseDouble(JOptionPane.showInputDialog("Informe a largura do terreno:"));
+        comprimento = Double.parseDouble(JOptionPane.showInputDialog("Informe a comprimento do terreno:"));
+        valor = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor por metro quadrado:"));
+
+        area = largura * comprimento;
+        valor = valor * area;
+
+        DecimalFormat decimal = new DecimalFormat("0.00");
+        JOptionPane.showMessageDialog(null, "Esse é o valor do seu terreno: " + decimal.format(valor));
+
+
+    }
+}
